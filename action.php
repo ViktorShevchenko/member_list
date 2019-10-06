af66d153c7cf255bd04c6e8dfde7db49db605b27
Viktor Shevchenko <viktor.shevchenko32@gmail.com>
2019-10-07 01:46:10 +0300

    Update action.php

--- action.php
+++ action.php
@@ -3,13 +3,17 @@
 include('database_connection.php');
 
 if (isset($_POST["action"])) {
-    
-    if ($_POST["action"] == "insert") {
-        $query = "
-        INSERT INTO tbl_sample (first_name, second_name, email) VALUES ('".$_POST["first_name"]."', '".$_POST["second_name"]."', '".$_POST["email"]."')
-        ";
-        $statement = $connect->prepare($query);
-        $statement->execute();
+
+    $correct_input = ctype_alnum($_POST["first_name"]) && ctype_alnum($_POST["second_name"]) && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
+
+    if($correct_input) {
+        if ($_POST["action"] == "insert") {
+            $query = "
+            INSERT INTO tbl_sample (first_name, second_name, email) VALUES ('".$_POST["first_name"]."', '".$_POST["second_name"]."', '".$_POST["email"]."')
+            ";
+            $statement = $connect->prepare($query);
+            $statement->execute();
+        }
     }
     
     if ($_POST["action"] == "fetch_single") {
@@ -28,15 +32,17 @@ if (isset($_POST["action"])) {
     }
 
     if ($_POST["action"] == "update") {
-        $query = "
-        UPDATE tbl_sample 
-        SET first_name = '".$_POST["first_name"]."',
-        second_name = '".$_POST["second_name"]."', 
-        email = '".$_POST["email"]."' 
-        WHERE id = '".$_POST["hidden_id"]."'
-        ";
-        $statement = $connect->prepare($query);
-        $statement->execute();
+        if($correct_input) {
+            $query = "
+            UPDATE tbl_sample
+            SET first_name = '".$_POST["first_name"]."',
+            second_name = '".$_POST["second_name"]."',
+            email = '".$_POST["email"]."'
+            WHERE id = '".$_POST["hidden_id"]."'
+            ";
+            $statement = $connect->prepare($query);
+            $statement->execute();      
+        }
     }
 
     if ($_POST["action"] == "delete") {
